d29483742be10fd4e6e5ecd58135e4a1ce7115f1
S4NT14G0V <100742956+S4NT14G0V@users.noreply.github.com>
2025-08-18 15:55:04 -0500

    [FEAT] Added modules folder

---
+++ src/app/account/setup/page.tsx
@@ -0,0 +1,7 @@
+import AccountSetup from "@/modules/employee/pages/AccountSetup"
+
+export default function Home() {
+  return (
+    <AccountSetup />
+  );
+}
---
+++ src/modules/employee/pages/AccountSetup.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AccountSetup() {
+  return (
+    <main className="min-h-[calc(100vh-48px)] flex items-center justify-center">
+      account setup page
+    </main>
+  )
+}
